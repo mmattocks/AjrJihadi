c23e397447e3716b344cf2c16c1487951f7dff64
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-03-05 10:32:10 -0500

    remove onadd and onmodify syncing, server task process to take care

--- hooks/on-modify-ajr.fish
+++ hooks/on-modify-ajr.fish
@@ -3,7 +3,6 @@ read --line json1 json2
 set parent (echo $json | jq '.parent // empty')
 if test -n "$parent"
     python3 ~/.task/hooks/ajrjihadi/modify-ajr.py "$json1" "$json2"
-    task sync
 else
     echo $json2
 end
