fcbb08e01b62a9e640beed593be7055057a85629
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-03-06 13:13:32 -0500

    missed prayer debug

--- hooks/ajrjihadi/display_missed.py
+++ hooks/ajrjihadi/display_missed.py
@@ -4,14 +4,14 @@ prayer_list=['Maghrib', 'Isha', 'Fajr', 'Dhuhr', 'Asr']
 
 db=TaskWarrior()
 
-for prayer in prayer_list
-    p_task=db.tasks.get(uuid=new_task['parent'])
+for prayer in prayer_list:
+    p_task=db.tasks.get(description=prayer, project='Din.Salat', status='Recurring')
     found_missed = False
     for p_ann in p_task['annotations']:
         ann_split = p_ann.split(":")
         if len(ann_split) == 2 and ann_split[0] == 'MISSED' and ann_split[1].isnumeric() and ann_split[1] > 0:
             found_missed = True
             print(prayer + " : " + str(ann_split[1]))
-        else:
-            print(prayer + " : " + "0")
+    if not found_missed:
+        print(prayer + " : " + "0")
 
--- hooks/ajrjihadi/makeup.py
+++ hooks/ajrjihadi/makeup.py
@@ -2,21 +2,34 @@ import sys
 from tasklib import TaskWarrior
 
 prayer_list=['Maghrib', 'Isha', 'Fajr', 'Dhuhr', 'Asr']
-prayer=sys.argv[1]
+pstring=''
+for prayer in prayer_list:
+    pstring+=prayer + ' '
 
-if prayer in prayer_list:
-    db=TaskWarrior()
-    p_task=db.tasks.get(description=prayer, project='Din.Salat', status='Recurring')
-    found_missed=False
-    for p_ann in p_task['annotations']
-        ann_split = p_ann.split(":")
-        if len(ann_split) == 2 and ann_split[0] == 'MISSED' and ann_split[1].isnumeric() and ann_split[1] > 0:
-            found_missed = True
-            ann_split[1] -= 1
-            p_task.remove_annotation(p_ann)
-            p_task.add_annotation(ann_split[0]+':'+ann_split[1])
-    if not found_missed:
-        print("No missed prayers recorded for " + prayer)
-    p_task.save()    
-else:
-    print(prayer + ' not in prayer list.')
+if len(sys.argv) == 1:
+    sys.exit("Which prayer was made up? " + pstring)
+elif len(sys.argv) > 3 or len(sys.argv) == 3 and not sys.argv[2].isnumeric():
+    sys.exit("Bad arguments. use 'makeup <prayer>' or 'makeup <prayer> <integer>'")
+
+num_madeup=1
+prayer=sys.argv[1]
+if len(sys.argv) == 3:
+    num_madeup=sys.argv[2]
+
+if prayer not in prayer_list:
+    sys.exit(str(prayer) + ' not in prayer list: ' + pstring)
+    
+db=TaskWarrior()
+p_task=db.tasks.get(description=prayer, project='Din.Salat', status='Recurring')
+found_missed=False
+for p_ann in p_task['annotations']:
+    ann_split = p_ann.split(":")
+    if len(ann_split) == 2 and ann_split[0] == 'MISSED' and ann_split[1].isnumeric() and ann_split[1] > 0:
+        found_missed = True
+        ann_split[1] -= num_madeup
+        p_task.remove_annotation(p_ann)
+        p_task.add_annotation(ann_split[0]+':'+ann_split[1])
+        print
+if not found_missed:
+    print("No missed prayers recorded for " + prayer)
+p_task.save()    
