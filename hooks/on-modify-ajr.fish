1b8a63752d6822b66d714a5bfdda5ca10ed52753
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-03-05 23:22:16 -0500

    on-modify-ajr fix

--- hooks/on-modify-ajr.fish
+++ hooks/on-modify-ajr.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 read --line json1 json2
-set parent (echo $json | jq '.parent // empty')
+set parent (echo $json1 | jq '.parent // empty')
 if test -n "$parent"
     python3 ~/.task/hooks/ajrjihadi/modify-ajr.py "$json1" "$json2"
 else
