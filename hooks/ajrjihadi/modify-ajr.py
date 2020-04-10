630b516bdbc8cc0e240a80bd2533c7e47764d49f
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-04-09 22:26:52 -0400

    updated on-ajr and modify-ajr

--- hooks/ajrjihadi/modify-ajr.py
+++ hooks/ajrjihadi/modify-ajr.py
@@ -1,28 +1,31 @@
 import sys
 import json
-import os.path
 import usrconfig
 
-new_task = json.loads(sys.argv[1])
+old_task = json.loads(sys.argv[1])
+new_task = json.loads(sys.argv[2])
 
-if usrconfig.track_missed and new_task['status'] == 'deleted' and new_task['description'] in usrconfig.prayer_list:
-    from tasklib import TaskWarrior
+missed=0
 
-    db=TaskWarrior()
-    p_task=db.tasks.get(uuid=new_task['parent'])
-    found_missed = False
-    for p_ann in p_task['annotations']:
-        ann_split = p_ann['description'].split(":")
-        if len(ann_split) == 2 and ann_split[0] == 'MISSED' and ann_split[1].isnumeric():
-            found_missed = True
-            new_missed = int(ann_split[1]) + 1
-            p_task.remove_annotation(p_ann)
-            p_task.add_annotation(ann_split[0]+':'+str(new_missed))
+if 'mask' in old_task.keys() and 'mask' in new_task.keys():
+    missed = new_task['mask'].count('X') - old_task['mask'].count('X')
+
+if missed > 0:
+    found_missed=False
+    if 'annotations' in new_task.keys():
+        for ann in new_task['annotations']:
+            ann_split = ann['description'].split(":")
+            if len(ann_split) == 2 and ann_split[0] == 'MISSED' and ann_split[1].isnumeric():
+                found_missed = True
+                ann['description'] = 'MISSED:'+str(int(ann_split[1]) + missed)
     if not found_missed:
-        p_task.add_annotation('MISSED:'+str(usrconfig.missed_start+1))
-    p_task.save()
+        from datetime import datetime
+        if 'annotations' in new_task.keys():
+            new_task['annotations'].append({'entry': datetime.now().isoformat(), 'description': 'MISSED:'+str(usrconfig.missed_start+missed)})
+        else:
+            new_task['annotations']=[{'entry': datetime.now().isoformat(), 'description': 'MISSED:'+str(usrconfig.missed_start+missed)}]
     print(json.dumps(new_task))
 else:
     print(json.dumps(new_task))
-    
+
 sys.exit(0)
--- hooks/on-modify-ajr.fish
+++ hooks/on-modify-ajr.fish
@@ -1,8 +1,10 @@
 #!/usr/bin/fish
 read --line json1 json2
-set parent (echo $json2 | jq '.parent // empty')
-if test -n "$parent"
-    python3 ~/.task/hooks/ajrjihadi/modify-ajr.py "$json2"
+set tstatus (echo $json1 | jq -r '.status // empty')
+set desc (echo $json1 | jq -r '.description // empty')
+if test $tstatus = 'recurring'
+    and contains $desc $salat_names
+    python3 ~/.task/hooks/ajrjihadi/modify-ajr.py "$json1" "$json2"
 else
     echo $json2
 end
