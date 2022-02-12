fb7abfb93de5e566c2eff020bad4f04d438f2b32
Michael Mattocks <mike.mattocks@utoronto.ca>
2022-02-11 23:41:08 -0500

    recurrence shift prototype

--- hooks/ajrjihadi/add-ajr.py
+++ hooks/ajrjihadi/add-ajr.py
@@ -18,6 +18,18 @@ if task['description'] in usrconfig.prayer_list:
 
     if descmod != '': #if there's a modified description, assign it
         task['description'] = descmod
+
+else:
+    from tasklib import TaskWarrior
+    db=TaskWarrior()
+    p_task=db.tasks.get(uuid=task['parent'])
+    due_shift = task['due'] - p_task['due']
+
+    time_attributes = ('wait', 'scheduled', 'until')
+    for att in time_attributes:
+        if p_task[attr]:
+            task[attr] = p_task[attr] + due_shift
+
 elif usrconfig.recur_cull:
     from datetime import datetime, timedelta
     from ajrSalat import tw_ISO8601_to_local_dt, dateTimeFormat
